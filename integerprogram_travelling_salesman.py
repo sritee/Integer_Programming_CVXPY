5eb6be7e62032fa081a86925654a62c7106c70c8
Sridhar Thiagarajan <thiagarajan14111@mech.ssn.edu.in>
2018-11-09 12:37:30 -0800

    Update integerprogram_travelling_salesman.py

--- integerprogram_travelling_salesman.py
+++ integerprogram_travelling_salesman.py
@@ -5,12 +5,10 @@ Created on Thu Nov  8 15:25:33 2018
 
 @author: sritee
 """
-
 import cvxpy as cvx
 import numpy as np
 
 #np.random.seed(1)
-
 num_nodes=10#number of nodes in the TSP
 max_cost=10 #maximum cost per edge, for sampling.
 cost_matrix=np.random.randint(1,max_cost,size=(num_nodes,num_nodes)) #this will generate a random integer cost matrix
@@ -21,20 +19,13 @@ cost_matrix=cost_matrix + cost_matrix.T #ensure symmetry of the matrix
 #cost_matrix=np.array([[1,5,4,3],[3,1,8,2],[5,3,1,9],[6,4,3,4]]) #the true least cost is 12, this is used as a check.
 np.fill_diagonal(cost_matrix,100000) #make sure we don't travel from node to same node, by having high cost.
 
-
 x=cvx.Variable((num_nodes,num_nodes),boolean=True) #x_ij is 1, if we travel from i to j in the tour.
-
 u=cvx.Variable(num_nodes) #variables in subtour elimination constraints
 
-
 cost=cvx.trace(cvx.matmul(cost_matrix.T,x)) #total cost of the tour
-
-
-
 ones_arr=np.ones([num_nodes]) #array for ones
   
 constraints=[]
-
 #now, let us make sure each node is visited only once, and we leave only once from that node.
 
 constraints.append(cvx.matmul(x.T,ones_arr)==1) 
@@ -55,8 +46,6 @@ prob.solve()
 
 #print(x.value.astype('int32'))
 #print(cost_matrix)
-
-
 
 tour=[0] #the final tour we have found will be stored here. Initialize with start node.
 verified_cost=0 #builds up the cost of the tour, independently from the gurobi solver. We use this as a sanity check.
